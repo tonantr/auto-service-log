246d1af70d3d250bb65b3a51dcbf3accba34bcc4
tonantr <tonantruong@gmail.com>
2025-02-23 09:49:29 -0500

    Added logging to AdminService.get_all_users()

--- backend/app/services/admin_service.py
+++ backend/app/services/admin_service.py
@@ -1,6 +1,21 @@
+import logging
 from app.models.user import User
+
+
+logging.basicConfig(
+    filename="app.log",
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(module)s - Line: %(lineno)d - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
 
 class AdminService:
     @staticmethod
     def get_all_users():
-        return User.query.all()
+        try:
+            return User.query.all()
+            logger.info("Successfully retrieved all users from the database.")
+        except Exception as e:
+            logger.error(f"Error in get_all_users: {str(e)}")
+            return []
